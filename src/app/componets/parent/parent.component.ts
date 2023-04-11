0e9ad3fba094151106f414ea23b01b788eacc5c0
nkunic <45877400+nkunic@users.noreply.github.com>
2023-04-11 21:22:50 +0200

    2 files modified

--- src/app/componets/parent/parent.component.ts
+++ src/app/componets/parent/parent.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemModel } from '../../model/item.model';
 
 @Component({
   selector: 'app-parent',
@@ -6,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ParentComponent {
 
-  items = [
-    'item1', 
-    'item2', 
-    'item3'
+  items: ItemModel[] = [
+    {
+      name: 'John',
+      surname: 'Doe'
+    },
+    {
+      name: 'Jane',
+      surname: 'Doe'
+    },
+    {
+      name: 'Max',
+      surname: 'Mustermann',
+      disabled: true
+    },
   ];
 
 }
